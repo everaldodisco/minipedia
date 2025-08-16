8a413bfb407e58ce791d510925770640d8292c6b
everaldodisco <everaldodisco@gmail.com>
2025-08-16 01:23:46 -0300

    Ajustes no projeto Minipedia

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
+<<<<<<< HEAD
 from flask_login import (
     LoginManager,
     UserMixin,
@@ -25,10 +26,20 @@ if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
     )
 
 # DB + Login
+=======
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minipedia.db'
+>>>>>>> 12a4e9b (Ajustes no projeto Minipedia)
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = "login"
 
+<<<<<<< HEAD
 # ---------------------
 # MODELOS
 # ---------------------
@@ -44,11 +55,24 @@ class Car(db.Model):
     brand = db.Column(db.String(100), nullable=False)
     model = db.Column(db.String(100), nullable=False)
     year = db.Column(db.Integer, nullable=False)
+=======
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(100))
+    model = db.Column(db.String(100))
+    year = db.Column(db.Integer)
+>>>>>>> 12a4e9b (Ajustes no projeto Minipedia)
 
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(User, int(user_id))
 
+<<<<<<< HEAD
 # ---------------------
 # SETUP INICIAL (tabelas + admin)
 # ---------------------
@@ -180,3 +204,56 @@ def server_error(e):
 # ---------------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+=======
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('Login inválido')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Usuário já existe')
+            return redirect(url_for('register'))
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Usuário registrado com sucesso!')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/colecao')
+@login_required
+def colecao():
+    cars = Car.query.all()
+    return render_template('colecao.html', cars=cars)
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+>>>>>>> 12a4e9b (Ajustes no projeto Minipedia)
---
+++ static/js/script.js
@@ -0,0 +1,1 @@
+console.log('Minipedia funcionando!');
