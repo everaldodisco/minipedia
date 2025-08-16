5ea2a4d6e9b6b790702f4c16847eae7332f06491
everaldodisco <everaldodisco@gmail.com>
2025-08-16 00:39:45 -0300

    Delete static directory

--- static/js/app.js
+++
@@ -1,3 +0,0 @@
-function alerta() {
-    alert("Botão funcionando com JS 🎉");
-}
--- static/js/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("clickBtn").addEventListener("click", function() {
-    alert("Botão funcionando! 🚀");
-});
